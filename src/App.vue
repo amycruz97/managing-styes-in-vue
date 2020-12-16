2939e73234e3cd82655cf5aacac32f85c61d6b13
amycruz97 <amarachiamaechi4@gmail.com>
2020-12-16 15:01:20 +0100

    Navbar component created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Navbar/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
+
+    
   }
 }
 </script>
@@ -19,8 +23,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+  
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="navbar">
+        
+        <div class="navLink">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar{
+    width: 100%;
+    background: #f44336;
+    padding: 1rem ;
+    font-size: 1.5rem;
+    
+
+}
+.navLink{
+
+text-align: center;
+}
+a{
+    text-decoration: none;
+    padding: 1rem;
+    color:#fff;
+    text-align: center;
+}
+
+@media only screen and (max-width: 600px) {
+    .navLink{
+        display: flex;
+        flex-direction: column;
+    }
+}
+</style>
